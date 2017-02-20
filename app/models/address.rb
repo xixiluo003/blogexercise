71ac2c52b5c9a1d4e50567d4c72ff70728762438
XiXi Luo <xixiluo003@gmail.com>
2017-02-20 15:18:52 -0500

    First pages commit

---
+++ app/models/address.rb
@@ -0,0 +1,4 @@
+class Address < ApplicationRecord
+  belongs_to :user
+  validates :street, :city, :state, :zip, presence: true
+end
---
+++ db/migrate/20170220174412_create_addresses.rb
@@ -0,0 +1,13 @@
+class CreateAddresses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :addresses do |t|
+      t.references :user, foreign_key: true
+      t.string :street
+      t.string :city
+      t.string :state
+      t.integer :zip
+
+      t.timestamps
+    end
+  end
+end
